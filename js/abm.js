20016f9bb0c06efa97c5b44afe17077044544456
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-03-02 17:55:52 -0300

    feat: metodos de array + dom

--- js/abm.js
+++ js/abm.js
@@ -21,7 +21,18 @@ function agregar() {
 }
 
 function mostrar() {
-    const div = document.getElementById('alumnos');
+    const div = document.getElementById('alumnos'); //Parent
     
-    div.innerHTML = JSON.stringify(personas);
+    div.innerHTML = '';
+
+    //for of
+    for(let item of personas) {
+        //crear el elemento 
+        const h = document.createElement('h1');
+        h.textContent = `${item.nombre} ${item.apellido}`;
+        
+        //agregar al nodo padre
+        div.appendChild(h);
+    }
+    //personas.forEach(x => div.innerHTML +=  x.nombre + ' ' + x.apellido + '<br>');
 }
